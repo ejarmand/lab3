2666f37debbe3d42a9a9beb32f58a45442720ba4
ejarmand <earmand@ucsd.edu>
2019-01-24 10:10:07 -0800

    latest  commit

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,13 +5,23 @@ $(document).ready(function() {
 	initializePage();
 })
 
+function projectClick(e) {
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+	containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+	$(".project-description").fadeToggle()
+}
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("A new message!!");
+
+		$(".jumbotron p").toggleClass("active");
+
 	});
+	$("a.thumbnail").click(projectClick)
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
